47ba2835168e0f2350e83fbb5de30290add7b7ae
Artur <arturpereiralino@gmail.com>
2026-08-19 09:33:45 -0300

    Segundo exercício adicionado

---
+++ ex2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+  char nome [50];
+  int idade;
+  
+  printf("Digite seu nome:");
+  scanf("%s",nome);
+  prinf("Digite sua idade:");
+  scanf("%d", idade);
+  prinf("Seu nome é %s, sua idade é %d", nome, idade)
+
+}
